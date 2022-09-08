432a503576864c95d5566b7e7c29f7e7d1778336
Ancelin Bouchet <ancelin@bouchet.info>
2022-09-08 13:46:42 +0200

    feat: Implemented pipe macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,38 @@
+#[macro_export]
+macro_rules! pipe {
+    (@wrap $func:expr, $($tail:expr),*) => {
+        ($func)(pipe!(@wrap $($tail),*))
+    };
+    (@wrap $x:expr) => {
+        $x
+    };
+    (@reverse [] $($reversed:expr),* $(,)?) => {
+        pipe!(@wrap $($reversed),*)
+    };
+    (@reverse [$head:expr, $($tail:expr),+] $($reversed:expr),* $(,)?) => {
+        pipe!(@reverse [$($tail),+] $head, $($reversed),*)
+    };
+    (@reverse [$head:expr] $($reversed:expr),* $(,)?) => {
+        pipe!(@reverse [] $head, $($reversed),*)
+    };
+    ($x:expr) => {
+        $x
+    };
+    ($x:expr $(=> $functions:expr)+) => {
+        pipe!(@reverse [$x, $($functions),+])
+    };
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn pipe_macro_result() {
+        // Given
+        let some_string = "32";
+        let expected_result = 32;
+        // When
+        let r = pipe!(some_string => str::parse::<i32> => Result::unwrap);
+        // Then
+        assert_eq!(r, expected_result, "We are testing that the result given by the expansion of the macro 'r' (value: {}) is equal to our expected value: {}", r, expected_result);
     }
 }
