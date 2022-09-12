f043a5cca4a41c5abeb0cc0a0d8a00fc22fb76eb
Ancelin Bouchet <ancelin@bouchet.info>
2022-09-12 12:56:22 +0200

    doc: Added documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,39 @@
+//! This crate provides a `pipe` macro to easily compose functions together
+//! with a new syntax preventing nested calls which are hard to read and understand.
+//!
+//! example usage:
+//! ```
+//! use another_pipe_macro::pipe;
+//!
+//! let res = pipe!( "32" => str::parse::<i32> => Result::unwrap);
+//!
+//! assert_eq!(res, 32);
+//! ```
+//!
+//! ## Should you use this crate?
+//! No.
+//!
+//! It is meant as an exercice in writing macros and publishing to crates.io, and
+//! possibly lacks features. If you want a more mature crate, use the [pipeline](https://crates.io/crates/pipeline) crate.
+
 #[macro_export]
+/// The `pipe` macro allows a new syntax to easily compose functions together.
+///
+/// example:
+/// ```
+/// use another_pipe_macro::pipe;
+///
+/// let res = pipe!(
+///     "30"
+///     => str::parse::<i32>
+///     => Result::unwrap // works with methods using function call syntax
+///     => |x| x + 2 // works with closures
+/// );
+///
+/// assert_eq!(res, 32);
+/// ```
+///
+/// The above example's `pipe` macro invocation expands to `(|x| x + 2)((Result::unwrap)((str::parse::<i32>)("30")))`
 macro_rules! pipe {
     (@wrap $func:expr, $($tail:expr),*) => {
         ($func)(pipe!(@wrap $($tail),*))
